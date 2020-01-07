36c353727d107cadb0ce1631d574f3b369112ef7
jeremy bultel <jerem.bultel@gmail.com>
2020-01-07 21:00:46 +0100

    !!

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guijustement c'était des.rubyonrails.org/routing.html
   root to: "cocktails#index"
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
